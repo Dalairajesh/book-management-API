6f24be3370092abc55bfb9242405c7af7f386aae
Dalairajesh <dalairajesh22@gmail.com>
2024-04-29 21:47:12 +0530

    change file name of user routes

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(express.json())
 const port = process.env.APP_PORT | 7000
 
 const bookRoute = require("./src/routes/book.routes")
-const userRoute = require("./src/routes/user.route")
+const userRoute = require("./src/routes/user.routes")
 
 
 app.use('/api/v1/book',bookRoute)
